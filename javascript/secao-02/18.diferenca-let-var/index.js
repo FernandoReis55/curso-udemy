42484aeedea0e036ffa0cd8f5b9e814c30fdc2ce
Fernando Reis <117377630+FernandoReis55@users.noreply.github.com>
2023-01-11 19:01:00 -0300

    adicionei mais exercicios

---
+++ javascript/secao-02/18.diferenca-let-var/index.js
@@ -0,0 +1,19 @@
+/* Dados Primitivos
+string, number, undefined, null, boolean.*/
+
+const nome = 'Fernando'; // string
+const nome1  = "Fernando"; //string
+const nome3 = `Fernando`; // string
+const num1 = 10; // number
+const num2 = 10.52; // number
+let nomeAluno; // undefined -> não aponta para local nenhuma na memória
+const sobrenomeAluno = null; // não aponta para local nenhuma na memória
+const aprovado = false; // Boolean = true, false (lógico)
+
+let a = 2;
+const b = a;
+console.log(a, b);
+
+a = 3;
+console.log(a, b);
+
---
+++ javascript/secao-02/20.operadores-aritmeticos/index.js
@@ -0,0 +1,49 @@
+/* Aritmeticos
+* + Adição /  concatenação - (unir 2 valores.)
+* ** Potenciação
+* % Resto da divisão
+*/
+
+/*
+Precedência dos valores
+()
+**
+* / %
++ -
+
+*/
+
+/*
+const num1 = 5;
+const num2 = 2;
+const num3 = 10;
+console.log((num1 + num2) * num3);
+
+// Contador++ - Incrementa valores.
+
+
+let contador = 1;
+++contador; // 2
+++contador; // 3
+++contador; // 4
+console.log(contador);
+
+// Contador-- - decremento
+
+ let contador = 10;
+ console.log(--contador);
+
+// operador de atribuição
+
+let contador = 2;
+contador *= 10;
+console.log(contador);
+
+
+// NaN - Not a number - parseInt (inteiro), parseFloat (decimais), Number não tem distinção.
+
+const num1 = 10;
+const num2 = Number ('5');
+console.log(num1 + num2);
+console.log(typeof num2);
+*/
