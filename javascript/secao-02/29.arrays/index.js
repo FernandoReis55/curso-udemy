0223e83beb8377841bee1eb20fa7bec950db01f8
Fernando Reis <117377630+FernandoReis55@users.noreply.github.com>
2023-01-23 18:48:00 -0300

    .

---
+++ javascript/secao-02/29.arrays/index.js
@@ -0,0 +1,39 @@
+//  Arrays - Lista ou coleção de coisas!!
+
+// indice     012345678910
+// const nome = 'Luiz Fernando';
+// console.log(nome[5]);
+
+//
+
+// indice         0       1           2
+const alunos = ['luiz', 'Fernando', 'Rocha',];
+
+
+
+//  console.log(alunos.slice(0, 2)); // Fatia os elementos.
+
+//  delete alunos[1]; // Deleta indice, fica um elemento vazio.
+//  console.log(alunos[1]);
+
+//  alunos.pop(); // Remove do fim.
+//  const removido = alunos.pop();
+//  console.log(removido);
+//  console.log(alunos);
+
+//  alunos.unshift('Pedro'); // Adiciona no começo.
+//  alunos.unshift('João');
+
+
+//  alunos.push('Pedro'); // Adiciona no fim.
+//  alunos.push('João');
+
+//  alunos[alunos.length] = 'Luiza'; // Adiciona no fim.
+
+//  alunos[2] = 'Reis'; // Trocando string por outra.
+//  alunos[3] = 'Galeno'; // Adicionando string no final.
+
+
+// console.log(alunos);
+// console.log(alunos[1]);
+// console.log(alunos[0]);
---
+++ javascript/secao-02/31.funcoes/index.js
@@ -0,0 +1,23 @@
+// Funcoes - Executa ação
+//  saudacao - Nome da função.
+// () - Valores / inputs
+//(nome) - Parametro
+// {} - Corpo
+
+
+/*function saudacao() {
+ padrão de codigo de função.
+}*/
+
+// function saudacao(nome) {
+//     return `Bom dia ${nome}!`;
+// }
+// Return é o fim da ação.
+// const variavel = saudacao('Fernando');
+// console.log(variavel);
+
+function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
+}
+console.log(soma(2, 3));
